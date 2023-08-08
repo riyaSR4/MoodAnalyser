75d60bb8979f2786dab9764550dd021bc4bfdf23
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-08-08 13:15:16 +0530

    [Riya] Add : Mood Analyser Exception

--- MoodAnalyserProblem/MoodAnalyser.cs
+++ MoodAnalyserProblem/MoodAnalyser.cs
@@ -11,12 +11,17 @@
         {
             try
             {
+                if (this.message == string.Empty)
+                    throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.EMPTY_MOOD,
+                        "Message is Empty");
                 if (message.Contains("Sad"))
                     return "SAD";
                 return "HAPPY";
-            }catch(NullReferenceException)
+            }
+            catch (NullReferenceException)
             {
-                return "HAPPY";
+                throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.NULL_MOOD,
+                     "Message is Null");
             }
         }
     }
